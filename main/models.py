fe58b86af144fbcc86a8facd91fe6e5510e83b86
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2023-08-22 15:47:40 +0600

    create RNRAccessToken model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,29 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+def uuid_without_dash():
+    return uuid.uuid4().hex
+
+
+# class RNRRefreshToken(models.Model):
+#     id = models.UUIDField(primary_key=True, unique=True, editable=False, default=uuid_without_dash)
+#     token = models.CharField(max_length=300, unique=True)
+#     expired = models.BooleanField(default=False)
+#     created = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return f"< Refresh-Token {self.token}>"
+
+
+class RNRAccessToken(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, editable=False, default=uuid_without_dash)
+    # rnr_refresh_token = models.ForeignKey(RNRRefreshToken, on_delete=models.CASCADE)
+    token = models.CharField(max_length=300, unique=True)
+    token_type = models.CharField(max_length=100, null=True)
+    expired = models.BooleanField(default=False)
+    expire_time = models.FloatField(default=3600.00)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"< Access-Token {self.token}>"
+

1cd4c31f44279b448275ed95b6e85189b795f0ae
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2023-08-24 16:25:13 +0600

    renamed from main to rnr

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'main.apps.MainConfig',
+    'rnr.apps.RNRConfig',
 
     'rest_framework',
     'rest_framework.authtoken',
--- rnr/apps.py
+++ rnr/apps.py
@@ -1,9 +1,9 @@
 from django.apps import AppConfig
 
 
-class MainConfig(AppConfig):
+class RNRConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'main'
+    name = 'rnr'
 
     def ready(self):
         from . import signals

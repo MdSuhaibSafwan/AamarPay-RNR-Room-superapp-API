186a8eb9156e59cb49e4ff470087181f7c9c4641
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2023-12-07 15:50:36 +0600

    gitignore fixed

--- rnr/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class RNRConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'rnr'
-
-    def ready(self):
-        from . import signals
